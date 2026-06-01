04ba99781cf13844e4bbecc2ab18412fed438a77
AbhishekPurbey03 <abhishekpurbey2002@gmail.com>
2026-06-01 13:24:44 +0530

    imported the library

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:frontend/main.dart';
+import 'package:vertex_bit/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
